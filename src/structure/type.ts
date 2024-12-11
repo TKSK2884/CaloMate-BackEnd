c57e3ae4e9b52098d60c74a67adae50f6d00bf41
Rufree <tksk2884@gmail.com>
2024-12-11 11:59:45 +0900

    인터페이스 CustomJwtPayload 추가

--- src/structure/type.ts
+++ src/structure/type.ts
@@ -1,4 +1,11 @@
+import { JwtPayload } from "jsonwebtoken";
+
 export interface UserInfo {
     id: number;
+    nickname: string;
+}
+
+export interface CustomJwtPayload extends JwtPayload {
+    id: string;
     nickname: string;
 }
