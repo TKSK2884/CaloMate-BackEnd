dc795ea56123d20c7a093db524e624485dbef2dd
Rufree <tksk2884@gmail.com>
2025-01-21 15:38:13 +0900

    몸무게 추가 관련 로직 추가

--- src/service/mypage.ts
+++ src/service/mypage.ts
@@ -45,6 +45,79 @@ export async function mypageProfileHandler(req: Request, res: any) {
     }
 }
 
+export async function mypageSaveProfileHandler(req: Request, res: any) {
+    try {
+        const user: JwtPayload | null = verifyAccessToken(
+            req.headers.authorization
+        );
+
+        const { weight } = req.body;
+
+        if (user == null) {
+            return res.status(400).json({
+                success: false,
+                error: "Invalid access token",
+            });
+        }
+
+        if (weight == null) {
+            return res.status(400).json({
+                success: false,
+                error: "Invalid weight info",
+            });
+        }
+
+        const userInfo = user as { id: number; nickname: string };
+
+        const [result] = await connectPool.query<mysql.RowDataPacket[]>(
+            "SELECT * FROM `profile` WHERE `user_id` = ? ORDER BY `created_at` DESC LIMIT 1",
+            [userInfo.id]
+        );
+
+        if (result.length === 0) {
+            return res.status(404).json({
+                success: false,
+                error: "No profile found",
+            });
+        }
+
+        const profile = result[0];
+
+        const [save] = await connectPool.query<mysql.ResultSetHeader>(
+            "INSERT INTO `profile` (`age`, `gender`, `height`, `weight`, `activityLevel`, `target`, `user_id`)" +
+                " VALUES (?, ?, ?, ?, ?, ?, ?)",
+            [
+                profile.age,
+                profile.gender,
+                profile.height,
+                weight,
+                profile.activityLevel,
+                profile.target,
+                userInfo.id,
+            ]
+        );
+
+        if (save.affectedRows > 0) {
+            console.log("프로필이 새로 추가되었습니다.");
+        } else {
+            console.error("프로필 추가 실패");
+            return res.status(400).json({
+                success: false,
+                error: "Profile failed to create",
+            });
+        }
+
+        return res.status(201).json({
+            success: true,
+        });
+    } catch {
+        return res.status(500).json({
+            success: false,
+            error: "Internal server error",
+        });
+    }
+}
+
 export async function mypagehistoryHandler(req: Request, res: any) {
     const user: JwtPayload | null = verifyAccessToken(
         req.headers.authorization
